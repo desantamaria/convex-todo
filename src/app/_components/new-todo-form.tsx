516b71fbae9e630b07ecc1b81ba18b8010ab44a9
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-09-26 15:12:22 -0700

    Create NewForm component

---
+++ src/app/_components/new-todo-form.tsx
@@ -0,0 +1,85 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+
+// Form Validation
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  description: z.string().min(2).max(50),
+});
+
+type ToDoFormProps = {
+  onCreate: (title: string, description: string) => void;
+};
+
+const NewTodoForm = ({ onCreate }: ToDoFormProps) => {
+  // Form Definition
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  // Define a submit handler
+  function onSubmit({ title, description }: z.infer<typeof formSchema>) {
+    onCreate(title, description);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              {/* <FormDescription>
+              This is your public display name.
+            </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              {/* <FormDescription>
+              This is your public display name.
+            </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="bg-slate-500 hover:bg-slate-700" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default NewTodoForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
 "use client";
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/Form";
-
 import { useState } from "react";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/Input";
 import { Checkbox } from "@/components/ui/Checkbox";
-
-const formSchema = z.object({
-  title: z.string().min(2).max(50),
-  description: z.string().min(2).max(50),
-});
+import NewTodoForm from "./_components/new-todo-form";
 
 type ToDoItem = {
   title: string;
@@ -34,22 +14,6 @@ export default function Home() {
   const [todos, setTodos] = useState<ToDoItem[]>([
     { title: "Example", description: "This is an example", completed: false },
   ]);
-
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      description: "",
-    },
-  });
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
 
   return (
     <div className="h-screen mx-auto p-4">
@@ -72,45 +36,17 @@ export default function Home() {
           </li>
         ))}
       </ul>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Title</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                {/* <FormDescription>
-                  This is your public display name.
-                </FormDescription> */}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                {/* <FormDescription>
-                  This is your public display name.
-                </FormDescription> */}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button className="bg-slate-500 hover:bg-slate-700" type="submit">
-            Submit
-          </Button>
-        </form>
-      </Form>
+      <NewTodoForm
+        onCreate={(title, description) => {
+          setTodos((prev) => {
+            const newTodos = [...prev];
+            newTodos.push({ title, description, completed: false });
+            return newTodos;
+          });
+        }}
+      />
     </div>
   );
 }
+
+// function ToDoItem({title, description, complete, onCompleteChanged})
