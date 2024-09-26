9381d9d5f82f40adbbfa4e953344dbffa055726b
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-09-26 14:34:04 -0700

    Create Todo From

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 "use client";
 
-import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+
 import { useState } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Checkbox } from "@/components/ui/Checkbox";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  description: z.string().min(2).max(50),
+});
 
 type ToDoItem = {
   title: string;
@@ -13,8 +34,25 @@ export default function Home() {
   const [todos, setTodos] = useState<ToDoItem[]>([
     { title: "Example", description: "This is an example", completed: false },
   ]);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div className="max-h-screen-md mx-auto p-4">
+    <div className="h-screen mx-auto p-4">
       <h1 className="text-xl font-bold">To-Do List</h1>
       <ul>
         {todos.map(({ title, description, completed }, index) => (
@@ -34,7 +72,45 @@ export default function Home() {
           </li>
         ))}
       </ul>
-      <form></form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="bg-slate-500 hover:bg-slate-700" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
